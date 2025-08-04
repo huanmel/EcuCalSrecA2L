ec50aecae6c0132abc5b21ccc1a679005796148c
ivanm <ivansmel@gmail.com>
2025-08-04 12:35:24 +0300

    fix imports

--- src/SrecFileUtils/srec_a2l_parser.py
+++ src/SrecFileUtils/srec_a2l_parser.py
@@ -3,18 +3,18 @@ from a2lparser.a2lparser import A2LParser
 from hexformat.srecord import SRecord
 from typing import Dict, Any, Union, List
 import pandas as pd
-
+from .format_map import A2L_FORMAT_MAP
 # from .format_map import A2L_FORMAT_MAP
-A2L_FORMAT_MAP = {
-    'UBYTE': {'format': 'B', 'length': 1},           # Unsigned 8-bit
-    'SBYTE': {'format': 'b', 'length': 1},           # Signed 8-bit
-    'UWORD': {'format': 'H', 'length': 2},           # Unsigned 16-bit
-    'SWORD': {'format': 'h', 'length': 2},           # Signed 16-bit
-    'ULONG': {'format': 'I', 'length': 4},           # Unsigned 32-bit
-    'SLONG': {'format': 'i', 'length': 4},           # Signed 32-bit
-    'FLOAT32_IEEE': {'format': 'f', 'length': 4},    # 32-bit float
-    'FLOAT64_IEEE': {'format': 'd', 'length': 8}     # 64-bit double
-}
+# A2L_FORMAT_MAP = {
+#     'UBYTE': {'format': 'B', 'length': 1},           # Unsigned 8-bit
+#     'SBYTE': {'format': 'b', 'length': 1},           # Signed 8-bit
+#     'UWORD': {'format': 'H', 'length': 2},           # Unsigned 16-bit
+#     'SWORD': {'format': 'h', 'length': 2},           # Signed 16-bit
+#     'ULONG': {'format': 'I', 'length': 4},           # Unsigned 32-bit
+#     'SLONG': {'format': 'i', 'length': 4},           # Signed 32-bit
+#     'FLOAT32_IEEE': {'format': 'f', 'length': 4},    # 32-bit float
+#     'FLOAT64_IEEE': {'format': 'd', 'length': 8}     # 64-bit double
+# }
 
 class SrecFileParser:
     """Class to parse and modify ECU firmware SREC and A2L files."""
